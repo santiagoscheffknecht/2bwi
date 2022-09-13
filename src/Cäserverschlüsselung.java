f0c0e520f73a98fc7835ef0c366e17294614fb12
santiago <santiago.scheffknecht@student.htldornbirn.at>
2022-09-13 11:24:32 +0200

    Santiagos aufgaben

--- src/Cäserverschlüsselung.java
+++ src/Cäserverschlüsselung.java
@@ -1,3 +1,19 @@
 public class Cäserverschlüsselung {
+    public static void main(String[] args)
+    {
+        char ch1 = 'i';
+        char ch2 = 'b';
+        int verschlüsselung = 2;
 
+        int asciivalue1 = ch1;
+        int asciivalue2 = ch2;
+
+        for (
+                
+        )
+        System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1 );
+        System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2 );
+
+
+    }
 }
