a7e8321677905b60128286abfe41c2af967ae01e
santiago <santiago.scheffknecht@student.htldornbirn.at>
2022-09-13 10:36:50 +0200

    Santiagos aufgaben

---
+++ src/TestTaskone.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class TestTaskone {
+    public static void main(String[] args) {
+        Random randomO = new Random();
+        int zahleins = randomO.nextInt(1000);
+        Random random1 = new Random();
+        int zahlzwei = random1.nextInt(1000);
+        if (zahleins < zahlzwei) {
+
+        }
+        ;
+        if (zahlzwei < zahleins) {
+
+
+
+        }
+    }
+
+
+}
