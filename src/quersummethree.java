7c96beb5f14e320415f2c6960d37f7437c99fef8
santiago <santiago.scheffknecht@student.htldornbirn.at>
2022-09-16 14:48:06 +0200

    Santiagos aufgaben

---
+++ src/quersummethree.java
@@ -0,0 +1,19 @@
+public class quersummethree {
+    public static void main(String[] args) {
+
+        for (int cnt = 0; cnt < 1000; cnt++) {
+            String numberAsString = Integer.toString(cnt);
+            char[] numbers = numberAsString.toCharArray();
+
+            int sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum = sum + Character.getNumericValue(numbers[i]);
+            }
+
+            if (cnt > sum) {
+
+                System.out.println("nummer" + cnt + "Quersumme" + sum);
+            }
+        }
+    }
+}
