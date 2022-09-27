1c799528ff04edb2acf5d3723e97d1d2995d2d18
santiago <santiago.scheffknecht@student.htldornbirn.at>
2022-09-27 10:53:28 +0200

    Santiagos aufgaben

--- src/quersummefour.java
+++ src/quersummefour.java
@@ -4,31 +4,46 @@ public class quersummefour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("start the calculator press 1");
-        System.out.println("end the calculator press 2:");
+        System.out.println("was ist deine Nummer");
+
+
+        int selection1 = scanner.nextInt();
+        System.out.println(selection1);
+
+
+        int cnt = selection1;
         boolean isFinished = false;
+        int sum = 0;
         while (!isFinished) {
-
-            int selection1 = scanner.nextInt();
-            System.out.println(selection1);
-
-            int cnt = 0;
             String numberAsString = Integer.toString(cnt);
             char[] numbers = numberAsString.toCharArray();
 
-            int sum = 0;
+
             for (int i = 0; i < numbers.length; i++) {
                 sum = sum + Character.getNumericValue(numbers[i]);
             }
 
-            if (selection1 == 1) {
-                int selection2 = scanner.nextInt();
-                cnt = selection2;
-                System.out.println(sum);
+            System.out.println(sum);
+
+            if (sum >= 10) {
+                cnt = sum;
+                sum = 0;
+                isFinished = false;
+                int x = selection1;
+                int y = cnt;
+                System.out.println(x+y);
+
+            } else {
+
+                isFinished = true;
             }
+            ;
+
+
         }
     }
 }
+
 
 
 
