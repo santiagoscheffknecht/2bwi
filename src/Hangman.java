88b11faa825ab6d9972cc5d07b229263c1ce0b87
santiago <santiago.scheffknecht@student.htldornbirn.at>
2022-10-18 09:55:50 +0200

    Santiagos aufgaben

---
+++ src/Hangman.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        Scanner buchstabe = new Scanner(System.in);
+    }
+}
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 import java.util.*;
+
 public class TicTacToe {
-    public static void main(String[] args) {;
+    public static void main(String[] args) {
+        ;
         //Feld erstellen
         char[][] field = new char[3][3];
         for (int i = 0; i < 3; i++) {
@@ -10,118 +12,111 @@ public class TicTacToe {
             field[2][i] = '+';
         }
         for (int i = 0; i < field.length; i++) {
-            System.out.println(field[i]);};
+            System.out.println(field[i]);
+        }
+        ;
         System.out.println("turn x");
 
         boolean isFinished = false;
         int round = 0;
 
         while (!isFinished) {
-        Scanner position = new Scanner(System.in);
+            Scanner position = new Scanner(System.in);
 
-        //position suchen
-        String input = position.next();
-        String[] in = input.split("/");
-        int x = Integer.parseInt(in[0]);
-        int y = Integer.parseInt(in[1]);
-        if (round %2 == 0){
-            field[x][y] = 'x';
-        }
-        else {
-            field[x][y] = 'o';
-        }
+            //position suchen
+            String input = position.next();
+            String[] in = input.split("/");
+            int x = Integer.parseInt(in[0]);
+            int y = Integer.parseInt(in[1]);
+            if (round % 2 == 0) {
+                field[x][y] = 'x';
+            } else {
+                field[x][y] = 'o';
+            }
 
             for (int i = 0; i < field.length; i++) {
                 System.out.println(field[i]);
             }
-        round++;
-        if (round %2 == 0){
-            System.out.println("turn x");
-        }
-        else {
-            System.out.println("turn o");
-        }
+            round++;
+            if (round % 2 == 0) {
+                System.out.println("turn x");
+            } else {
+                System.out.println("turn o");
+            }
 
-        if (field[0][0] == field[1][1] && field[2][2] == field[1][1] && field[1][1] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-            }
-            isFinished = true;
-        }
-        else if (field[0][0] == field[0][1] && field[0][2] == field[0][1] && field[0][1] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-            isFinished = true;
-            }
-        }
-        else if (field[1][0] == field[1][1] && field[1][2] == field[1][1] && field[1][1] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
+            if (field[0][0] == field[1][1] && field[2][2] == field[1][1] && field[1][1] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                }
                 isFinished = true;
-            }
+            } else if (field[0][0] == field[0][1] && field[0][2] == field[0][1] && field[0][1] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (field[1][0] == field[1][1] && field[1][2] == field[1][1] && field[1][1] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
 
+            } else if (field[2][0] == field[2][1] && field[2][2] == field[2][1] && field[2][1] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (field[0][0] == field[2][0] && field[2][0] == field[1][0] && field[2][0] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (field[0][1] == field[2][1] && field[2][1] == field[1][1] && field[2][1] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (field[0][2] == field[2][2] && field[2][2] == field[1][2] && field[2][2] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (field[0][0] == field[2][0] && field[2][0] == field[1][0] && field[2][0] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (field[0][2] == field[1][1] && field[2][0] == field[1][1] && field[1][1] != '+') {
+                if (round % 2 == 0) {
+                    System.out.println("o has won se game");
+                } else {
+                    System.out.println("x has won se game");
+                    isFinished = true;
+                }
+            } else if (round == 9) {
+                System.out.println("you both lost there is no draw");
+                isFinished = true;
+
+            }
+            ;
+            ;
+
+            ;
+        }
+    }
 }
-        else if (field[2][0] == field[2][1] && field[2][2] == field[2][1] && field[2][1] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-                isFinished = true;
-            }}
-        else if (field[0][0] == field[2][0] && field[2][0] == field[1][0] && field[2][0] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-                isFinished = true;
-            }}
-        else if (field[0][1] == field[2][1] && field[2][1] == field[1][1] && field[2][1] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-                isFinished = true;
-            }}
-        else if (field[0][2] == field[2][2] && field[2][2] == field[1][2] && field[2][2] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-                isFinished = true;
-            }}
-        else if (field[0][0] == field[2][0] && field[2][0] == field[1][0] && field[2][0] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-                isFinished = true;
-            }}
-        else if (field[0][2] == field[1][1] && field[2][0] == field[1][1] && field[1][1] != '+') {
-            if (round %2 == 0){
-                System.out.println("o has won se game");
-            }
-            else {
-                System.out.println("x has won se game");
-                isFinished = true;
-            }}
-        else if (round == 9){
-            System.out.println("you both lost there is no draw");
-            isFinished = true;
-
-        };;
-
-            ;}}}
