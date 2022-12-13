c6f5513c183f822161d3f93b3e85dbd10488c144
santiago <santiago.scheffknecht@student.htldornbirn.at>
2022-12-13 11:04:28 +0100

    OO Autoerwieterungen begonnen und beendet

--- src/at/santiago/java/oo/car/Car.java
+++ src/at/santiago/java/oo/car/Car.java
@@ -5,12 +5,36 @@ public class Car {
 
     public int fuelConsumption;
     public int fuelAmount;
+    public int maxfuelAmount;
     public String brand;
     public String serialnumber;
     private String color;
+    private String horn = "tuuut";
+
+
+
 
     public void drive() {
         this.fuelAmount = this.fuelAmount - fuelConsumption;
         System.out.println("I'm driving");
+    }
+    public void breake(){
+        System.out.println("ich bremse");
+    }
+    public void turboBoost(){
+        if (fuelAmount>=maxfuelAmount*0.1){
+            System.out.println("SuperBoostMode");
+        }
+        else {
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
+    public void honk(int amountOfRepitions){
+        for (int i = 0; i < amountOfRepitions; i++) {
+            System.out.println(horn);
+        }
+    }
+    public void getRemainingRange(){
+        System.out.println(fuelAmount/fuelConsumption + " times to dirve");
     }
 }
--- src/at/santiago/java/oo/car/Main.java
+++ src/at/santiago/java/oo/car/Main.java
@@ -9,16 +9,23 @@ public class Main {
         C1.fuelConsumption = 7;
         C1.serialnumber = "A7";
         C1.fuelAmount = 70;
+        C1.maxfuelAmount = 70;
 
         Car C2 = new Car();
         C2.brand = "BMW";
         C2.fuelConsumption = 5;
         C2.serialnumber = "Bin Moslem Weisch";
         C2.fuelAmount = 69;
+        C2.maxfuelAmount = 69;
 
-        System.out.println(C2.fuelAmount);
-        C2.drive();
-        System.out.println(C2.fuelAmount);
+        System.out.println(C1.fuelAmount);
+        System.out.println(C1.fuelAmount);
+        C1.breake();
+        C1.turboBoost();
+        C1.honk(3);
+        C1.getRemainingRange();
+        C1.drive();
+        C1.getRemainingRange();
     }
 
 }
