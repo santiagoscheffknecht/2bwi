3a2d56ddf9c2081fac192e99013549dcf0eca4fd
santiago <santiago.scheffknecht@student.htldornbirn.at>
2023-01-10 11:19:26 +0100

    Refactoring gestarted

--- src/at/santiago/java/oo/car/Car.java
+++ src/at/santiago/java/oo/car/Car.java
@@ -3,25 +3,35 @@ package at.santiago.java.oo.car;
 public class Car {
     //Instanz / Gedächnissvariablen
 
-    public int fuelConsumption;
-    public int fuelAmount;
-    public int maxfuelAmount;
-    public String brand;
-    public String serialnumber;
+    private int fuelConsumption;
+    private int fuelAmount;
+    private int maxfuelAmount;
+    private String brand;
+    private String serialnumber;
     private String color;
     private String horn = "tuuut";
 
-    public Car(int fuelConsumption, String brand, String serialNumber)
+    public Car(int fuelConsumption, String brand, String serialNumber, int fuelAmount, int maxfuelAmount)
     {this.fuelConsumption = fuelConsumption;
     this.brand = brand;
-    this.serialnumber = serialNumber;};
+    this.serialnumber = serialNumber;
+    this.fuelAmount =  fuelAmount;
+    this.maxfuelAmount = maxfuelAmount;
+    }
+
+
+    //Drive
     public void drive() {
         this.fuelAmount = this.fuelAmount - fuelConsumption;
         System.out.println("I'm driving");
     }
+
+    //Breake
     public void breake(){
         System.out.println("ich bremse");
     }
+
+    //turboBoost
     public void turboBoost(){
         if (fuelAmount>=maxfuelAmount*0.1){
             System.out.println("SuperBoostMode");
@@ -30,12 +40,72 @@ public class Car {
             System.out.println("Not enough fuel to go to Superboost");
         }
     }
+
+    //honk
     public void honk(int amountOfRepitions){
         for (int i = 0; i < amountOfRepitions; i++) {
             System.out.println(horn);
         }
     }
+
+    //remaining Range to drive
     public void getRemainingRange(){
         System.out.println(fuelAmount/fuelConsumption + " times to dirve");
     }
+
+    //setter für Gedächnisinstanz
+
+
+    public void setFuelAmount(int fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMaxfuelAmount(int maxfuelAmount) {
+        this.maxfuelAmount = maxfuelAmount;
+    }
+
+    public void setSerialnumber(String serialnumber) {
+        this.serialnumber = serialnumber;
+    }
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+    //getter für Klassen
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public int getMaxfuelAmount() {
+        return maxfuelAmount;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getHorn() {
+        return horn;
+    }
+
+    public String getSerialnumber() {
+        return serialnumber;
+    }
 }
+
--- src/at/santiago/java/oo/car/Main.java
+++ src/at/santiago/java/oo/car/Main.java
@@ -4,34 +4,20 @@ public class Main {
     public static void main(String[] args) {
         int a = 7;
 
-     /*   Car C1 = new Car();
-        C1.brand = "Audi";
-        C1.fuelConsumption = 7;
-        C1.serialnumber = "A7";
-        C1.fuelAmount = 70;
-        C1.maxfuelAmount = 70;
+        Car C1 = new Car(7,"Mercedes", "CL500", 100, 150){
 
-        Car C2 = new Car();
-        C2.brand = "BMW";
-        C2.fuelConsumption = 5;
-        C2.serialnumber = "Bin Moslem Weisch";
-        C2.fuelAmount = 69;
-        C2.maxfuelAmount = 69;
-
-      */
-
-        Car C3 = new Car(17, "Babawagen", "Apocar"){
         };
-/*
-        System.out.println(C1.fuelAmount);
-        System.out.println(C1.fuelAmount);
+        Car C3 = new Car(17, "Babawagen", "Apocar", 69, 100){
+        };
+
+        System.out.println(C1.getFuelAmount());
+        System.out.println(C3.getBrand());
         C1.breake();
         C1.turboBoost();
         C1.honk(3);
         C1.getRemainingRange();
         C1.drive();
         C1.getRemainingRange();
- */
     }
 
 }
